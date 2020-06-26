458286df608a637921bb1c5687ed1fb672df95e9
qzc123321 <qzcwork@126.com>
2020-06-26 11:37:36 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartAssistant
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ChromiumWebBrowser chromiumWebBrowser = new ChromiumWebBrowser();
+        public MainWindow()
+        {
+            InitializeComponent();
+            CefContainer.Children.Add(chromiumWebBrowser);
+            chromiumWebBrowser.Address = "https://www.baidu.com";
+        }
+    }
+}
